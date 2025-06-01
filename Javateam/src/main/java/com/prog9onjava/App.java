07f19403022c518d8b4ae6a8d75a8d7756dc5427
CHIRANTHAN610 <cchiranthan918@gmail.com>
2025-06-01 11:10:46 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/prog9onjava/App.java
@@ -0,0 +1,13 @@
+package com.prog9onjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
